25f48ff993358fd391e899771cb4aa6a3036fd2f
Mahad410 <mahadqureshi410@gmail.com>
2022-02-02 13:45:36 -0600

    login

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ button.addEventListener('click', () => {
             input[i].style.borderColor = 'red';
             notice[i].innerHTML = ' - This field is required';
             notice[i].style.visibility = 'visible';
+
         }
         return false;
-    }
-    if (email.indexOf('@') == -1) {
+    } else if (email.indexOf('@') == -1) {
         for (var i = 0; i < input.length; i++) {
             input[0].style.borderColor = 'red';
-            notice[0].innerHTML = ' - Invalid email'
+            notice[0].innerHTML = ' - Not a well formed email address.'
             notice[0].style.visibility = 'visible';
+
         }
+        return false;
+    } else if (password == '' || password == null || password == undefined) {
+        for (var i = 0; i < input.length; i++) {
+            input[1].style.borderColor = 'red';
+            notice[1].innerHTML = ' - This field is required';
+            notice[1].style.visibility = 'visible';
+
+        }
+        return false;
     } else {
+        alert("Sorry it's just a clone so not connected to database");
         return true;
     }
 })
